13fb702144daca8ae24c16b69042c280ec8a7c82
GiaTrong <hoanggiatrang01@gmail.com>
2023-12-16 21:26:44 +0700

    lam trang san pham products

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,27 @@
+const Product = require("../../models/product.modal");
+
 // module.exports.index : index is name of function
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+  // give all products in database
+  const products = await Product.find({
+    // status: "active",
+    // deleted: true
+  });
+  console.log(products);
+
+  // give a new price
+  const newProducts = products.map((item) => {
+    item.priceNew = (
+      (item.price * (100 - item.discountPercentage)) /
+      100
+    ).toFixed(0);
+
+    return item;
+  });
+
+  // render in viewer
   res.render("client/pages/products/index", {
-    pageTitle: "Danh sach san pham"
+    pageTitle: "Danh sach san pham",
+    products: newProducts,
   });
 };
