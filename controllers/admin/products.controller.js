40c35921819ef682f27247226e70891e8f66c022
GiaTrong <hoanggiatrang01@gmail.com>
2023-12-18 16:08:43 +0700

    lay data in ra giao dien

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,6 +1,17 @@
 // [GET] /admin/dashboard
-module.exports.index = (req, res) => {
+
+const Product = require("../../models/product.modal")
+
+module.exports.index = async (req, res) => {
+  // give a data product
+  const products = await Product.find({
+    deleted: false
+  })
+
+  // console.log(products)
+
   res.render("admin/pages/product/index", {
     pageTitle: "Trang san pham",
+    products: products
   });
 };
