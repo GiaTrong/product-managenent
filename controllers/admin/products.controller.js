802c86508888ba62d15a041c7aefd8a0de46af0b
GiaTrong <hoanggiatrang01@gmail.com>
2023-12-19 22:23:22 +0700

    tính năng phân trang

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -3,12 +3,11 @@
 // model database
 const Product = require("../../models/product.modal");
 // filter
-const filerStatusHelper = require("../../helpers/filterStatus")
+const filerStatusHelper = require("../../helpers/filterStatus");
 // search
-const searchStatusHelper = require("../../helpers/search")
+const searchStatusHelper = require("../../helpers/search");
 
 module.exports.index = async (req, res) => {
-  
   // FILTER => tách hẳn thằng filter ra 1 cái module riêng để
   // reuse cho dễ
   const filerStatus = filerStatusHelper(req.query);
@@ -32,12 +31,39 @@ module.exports.index = async (req, res) => {
   // }
   const objectSearch = searchStatusHelper(req.query);
   // console.log(objectSearch)
-  if(objectSearch.regex) {
+  if (objectSearch.regex) {
     find.title = objectSearch.regex;
   }
+  //  END search
+
+  // pagination
+  let objectPagination = {
+    currentPage: 1,
+    limitItem: 4,
+  };
+
+  if (req.query.page) {
+    objectPagination.currentPage = parseInt(req.query.page);
+  }
+
+  // skip = (trang hiện tại - 1) * số lượng phần tử mỗi trang
+  objectPagination.skip =
+    (objectPagination.currentPage - 1) * objectPagination.limitItem;
+
+  // amount of page
+  const countProduct = await Product.countDocuments(find);
+  const totalPage = Math.ceil(countProduct / objectPagination.limitItem); 
+  objectPagination.totalPage = totalPage;
+  // console.log(totalPage)
+
+  // END pagination
 
   // give a data product
-  const products = await Product.find(find);
+  // .limit(number) => chỉ lấy tưng đấy sản phẩm thôi
+  // skip: bỏ qua bao nhiêu sản phẩm
+  const products = await Product.find(find)
+    .limit(objectPagination.limitItem)
+    .skip(objectPagination.skip);
 
   // console.log(products)
 
@@ -46,6 +72,7 @@ module.exports.index = async (req, res) => {
     pageTitle: "Trang san pham",
     products: products,
     filerStatus: filerStatus,
-    keyword: objectSearch.keyword
+    keyword: objectSearch.keyword,
+    pagination: objectPagination
   });
 };
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,61 +1,80 @@
 // button status
-const buttonStatus = document.querySelectorAll("[button-status]")
+const buttonStatus = document.querySelectorAll("[button-status]");
 
-if(buttonStatus.length > 0) {
-    // give a URL
-    // and if you want to change or have a action with URL
-    // we have to use NEW URL(http.....)
-    let url = new URL(window.location.href);
+if (buttonStatus.length > 0) {
+  // give a URL
+  // and if you want to change or have a action with URL
+  // we have to use NEW URL(http.....)
+  let url = new URL(window.location.href);
 
-    buttonStatus.forEach(item => {
-        item.addEventListener("click", () => {
-            const status = item.getAttribute("button-status")
-            // console.log(status)
+  buttonStatus.forEach((item) => {
+    item.addEventListener("click", () => {
+      const status = item.getAttribute("button-status");
+      // console.log(status)
 
-            if(status) {
-                // những cái sau dấu hỏi => ngta gọi là searchParams
-                // if you have status, you will change the url
-                url.searchParams.set("status", status);
-            } else {
-                // or you dont have, remove KEY status in URL 
-                url.searchParams.delete("status");
-            }
+      if (status) {
+        // những cái sau dấu hỏi => ngta gọi là searchParams
+        // if you have status, you will change the url
+        url.searchParams.set("status", status);
+      } else {
+        // or you dont have, remove KEY status in URL
+        url.searchParams.delete("status");
+      }
 
-            // console.log(url.href)
+      // console.log(url.href)
 
-            // câu lệnh chuyển hướng
-            window.location.href = url.href
-        })
-    })
+      // câu lệnh chuyển hướng
+      window.location.href = url.href;
+    });
+  });
 }
 
 // end btn status
 
 // FORM SEARCH
-const formSearch = document.querySelector("#form-search")
+const formSearch = document.querySelector("#form-search");
 
 // console.log(formSearch)
-if(formSearch) {
+if (formSearch) {
+  //
+  let url = new URL(window.location.href);
+
+  formSearch.addEventListener("submit", (e) => {
+    // prevent move page
+    e.preventDefault();
     //
-    let url = new URL(window.location.href);
+    const keyword = e.target.elements.keyword.value;
+    //
+    if (keyword) {
+      // những cái sau dấu hỏi => ngta gọi là searchParams
+      // if you have status, you will change the url
+      url.searchParams.set("keyword", keyword);
+    } else {
+      // or you dont have, remove KEY status in URL
+      url.searchParams.delete("keyword");
+    }
 
-    formSearch.addEventListener("submit", (e) => {
-        // prevent move page
-        e.preventDefault();
-        //
-        const keyword = e.target.elements.keyword.value
-        //
-        if(keyword) {
-            // những cái sau dấu hỏi => ngta gọi là searchParams
-            // if you have status, you will change the url
-            url.searchParams.set("keyword", keyword);
-        } else {
-            // or you dont have, remove KEY status in URL 
-            url.searchParams.delete("keyword");
-        }
-
-        // câu lệnh chuyển hướng
-        window.location.href = url.href
-    })
+    // câu lệnh chuyển hướng
+    window.location.href = url.href;
+  });
 }
 // END FORM SEARCH
+
+// PAGINATION
+const buttonPagination = document.querySelectorAll("[button-pagination]");
+if (buttonPagination) {
+  let url = new URL(window.location.href);
+
+  buttonPagination.forEach((button) => {
+    button.addEventListener("click", () => {
+      const page = button.getAttribute("button-pagination");
+      //   console.log(page);
+
+      url.searchParams.set("page", page);
+
+      // câu lệnh chuyển hướng
+      window.location.href = url.href;
+    });
+  });
+}
+// END PAGINATION
